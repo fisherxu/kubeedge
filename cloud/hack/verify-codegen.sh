59c94304788122e8ca8ab2c3c7763232a41a174a
fisherxu <fisherxu1@gmail.com>
2020-01-05 22:06:31 +0800

    add reliablesync api

--- cloud/hack/verify-codegen.sh
+++ cloud/hack/verify-codegen.sh
@@ -18,9 +18,31 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd)
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
 
-${SCRIPT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
-github.com/kubeedge/kubeedge/cloud/pkg/client github.com/kubeedge/kubeedge/cloud/pkg/apis \
-"devices:v1alpha1 reliablesync:v1alpha1" \
---go-header-file ${SCRIPT_ROOT}/cloud/hack/boilerplate/boilerplate.txt
+DIFFROOT="${SCRIPT_ROOT}/pkg"
+TMP_DIFFROOT="${SCRIPT_ROOT}/_tmp/pkg"
+_tmp="${SCRIPT_ROOT}/_tmp"
+
+cleanup() {
+  rm -rf "${_tmp}"
+}
+trap "cleanup" EXIT SIGINT
+
+cleanup
+
+mkdir -p "${TMP_DIFFROOT}"
+cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
+
+"${SCRIPT_ROOT}/hack/update-codegen.sh"
+echo "diffing ${DIFFROOT} against freshly generated codegen"
+ret=0
+diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
+cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
+if [[ $ret -eq 0 ]]
+then
+  echo "${DIFFROOT} up to date."
+else
+  echo "${DIFFROOT} is out of date. Please run hack/update-codegen.sh"
+  exit 1
+fi
